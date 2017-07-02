2d028277079e746d0c38475c388d6125e79e68d0
Koen van Gilst <vnglst@users.noreply.github.com>
2017-07-02 19:39:58 +0200

    upgrades to React 15.6.1 and resolves warning (#37)

--- src/react-aria-modal.js
+++ src/react-aria-modal.js
@@ -166,9 +166,11 @@ let Modal = class Modal extends React.Component {
       dialogProps.tabIndex = '-1';
     }
 
-    const childrenArray = [React.DOM.div(dialogProps, props.children)];
+    const childrenArray = [React.createElement('div', dialogProps, props.children)];
     if (props.verticallyCenter) {
-      childrenArray.unshift(React.DOM.div(verticalCenterHelperProps));
+      childrenArray.unshift(
+        React.createElement('div', verticalCenterHelperProps)
+        );
     }
 
     return focusTrapFactory(
@@ -181,7 +183,7 @@ let Modal = class Modal extends React.Component {
           onDeactivate: this.deactivate
         }
       },
-      React.DOM.div(underlayProps, childrenArray)
+      React.createElement('div', underlayProps, childrenArray)
     );
   }
 }
